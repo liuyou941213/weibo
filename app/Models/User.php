757c74487569f7cb49400d373edf376c7f6dc198
liuyou <vagrant@homestead>
2020-06-21 14:56:44 +0000

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
+use Auth;
 
 class User extends Authenticatable
 {
@@ -61,11 +62,6 @@ class User extends Authenticatable
         return $this->hasMany(Status::class);
     }
 
-    public function feed()
-    {
-        return $this->statuses()->orderBy('created_at', 'desc');
-    }
-
     //获取用户粉丝列表
     public function followers()
     {
@@ -97,5 +93,12 @@ class User extends Authenticatable
     public function isFollowing($user_id)
     {
         return $this->followings->contains($user_id);
+    }
+
+    public function feed()
+    {
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+        return Status::whereIn('user_id', $user_ids)->with('user')->orderBy('created_at', 'desc');
     }
 }
